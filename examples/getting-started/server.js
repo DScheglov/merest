865dd1e524d3cf84563f595cb552c32c7addc7a6
Dmitry Scheglov <Dmitry.Scheglov@gmail.com>
2016-12-19 21:07:35 +0200

    v.1.2.0 - realese with documentation

--- examples/getting-started/server.js
+++ examples/getting-started/server.js
@@ -19,7 +19,7 @@ const app = express();
 // Creating the Express application to serve API
 const api = new merest.ModelAPIExpress({
   title: 'Contact List API',
-  host: 'ubuntu-local:8000', // Assign correct host that could be accessed from your network
+  host: 'localhost:8000', // Assign correct host that could be accessed from your network
   path: '/api/v1',
   options: false // we do not need the OPTIONS any more
 });
@@ -28,7 +28,10 @@ app.use(bodyParser.json()); // Parsing JSON-bodies
 app.use(methodOverride()); // Supporting HTTP OPTIONS and HTTP DELETE methods
 
 api.expose(Contact, { options: false }); // Exposing our API
-api.exposeSwaggerUi(); // Exposing swagger-ui
+api.exposeSwaggerUi('/api-ui', {
+  swaggerDoc: '/api-docs',
+  beautify: true
+}); // Exposing swagger-ui
 
 app.use('/api/v1', api); // mounting API as usual sub-application
 
