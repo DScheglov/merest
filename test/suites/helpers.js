65f9a51dce4e90c2591cc8a837d1470ceea18fb3
Dmitry Scheglov <Dmitry.Scheglov@gmail.com>
2017-02-17 18:51:08 +0200

    readonly on update and some codestyle updates

--- test/suites/helpers.js
+++ test/suites/helpers.js
@@ -81,4 +81,111 @@ describe('flat()', function () {
     assert.deepEqual(helpers.flat(obj), obj);
   });
 
-})
+  it ('should exclude some paths', function() {
+    var obj = {
+      x: '1',
+      y: 2,
+      z: {
+        a: 1,
+        b: {
+          c: 2
+        },
+        t: {
+          a: 7,
+          b: 8,
+          c: 9
+        }
+      }
+    };
+    var exclude = {
+      'z.a': true,
+      y: true,
+      'z.t': true
+
+    }
+    assert.deepEqual(helpers.flat(obj, exclude), {
+      x: 1,
+      'z.b.c': 2
+    });
+  });
+
+});
+
+describe('normalizeReadonly', function() {
+
+  it('should process string parameter', function() {
+    assert.deepEqual(
+      helpers.normalizeReadonly('a b c'),
+      { a: true, b: true, c: true }
+    )
+  });
+
+  it('should process array parameter', function() {
+    assert.deepEqual(
+      helpers.normalizeReadonly(['a', 'b', 'c']),
+      { a: true, b: true, c: true }
+    )
+  });
+
+  it('should process object parameter', function() {
+    assert.deepEqual(
+      helpers.normalizeReadonly({ a: true, b: true, c: true }),
+      { a: true, b: true, c: true }
+    )
+  });
+
+  it('should return null if no parameter passed', function() {
+    assert.equal(
+      helpers.normalizeReadonly(),
+      null
+    )
+  });
+
+  it('should return null if passed empty string', function() {
+    assert.equal(
+      helpers.normalizeReadonly(''),
+      null
+    )
+  });
+
+  it('should return null if passed empty array', function() {
+    assert.equal(
+      helpers.normalizeReadonly([]),
+      null
+    )
+  });
+
+  it('should return other default value if it is assigned and undefined passed', function() {
+    assert.deepEqual(
+      helpers.normalizeReadonly(undefined, { _id: true }),
+      { _id: true }
+    );
+  });
+
+  it('should return other default value if it is assigned and empty array passed', function() {
+    assert.deepEqual(
+      helpers.normalizeReadonly([], { _id: true }),
+      { _id: true }
+    );
+  });
+
+  it('should return other default value if it is assigned and number, true or date passed', function() {
+    assert.deepEqual(
+      helpers.normalizeReadonly(2, { _id: true }),
+      { _id: true }
+    );
+
+    assert.deepEqual(
+      helpers.normalizeReadonly(true, { _id: true }),
+      { _id: true }
+    );
+
+    assert.deepEqual(
+      helpers.normalizeReadonly(new Date('2017-01-01'), { _id: true }),
+      { _id: true }
+    );
+  });
+
+
+
+});
